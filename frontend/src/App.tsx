1ff9a7b34b88ec8f91434aa4fdb8bc253d959541
Pusztai Tamas <61682207+TomiGit98@users.noreply.github.com>
2022-06-16 12:46:30 +0200

    Registration and Login page sketch done
    Registration and Login page sketch done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { ContentPage } from './pages/ContentPage/ContentPage';
-import ProfilePage from './pages/Profile/ProfilePage';
-import { ProfilePageSmall } from './pages/ProfileSmall/ProfilePageSmall';
+//import { ContentPage } from './pages/ContentPage/ContentPage';
+//import ProfilePage from './pages/Profile/ProfilePage';
+//import { ProfilePageSmall } from './pages/ProfileSmall/ProfilePageSmall';
 import { LoginPage } from './pages/Unauthenticated/LoginPage/LoginPage';
 import { RegistrationPage } from './pages/Unauthenticated/RegistrationPage/RegistrationPage';
 
@@ -18,6 +18,17 @@ function App() {
       </div>*/}
       {/*<ContentPage />*/}
       {/*<LoginPage />*/}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />}>
+
+          </Route>
+          <Route path="/registration" element={<RegistrationPage />}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <RegistrationPage />
     </div>
   );
--- frontend/src/pages/Unauthenticated/RegistrationPage/RegistrationPage.tsx
+++ frontend/src/pages/Unauthenticated/RegistrationPage/RegistrationPage.tsx
@@ -1,29 +1,42 @@
 import "./RegistrationPage.scss"
 
+import snap from "../../../images/snap.png";
+
 export const RegistrationPage: React.FC = () => (
     <div className="registration-page-container">
         <div className="d-none d-sm-flex registration-page-container__registration-component--pc">
-            <p className="title-of-the-app">Register your account to Post It</p>
-            <input placeholder="e-mail, username" />
-            <input placeholder="password" />
-            <input placeholder="password again" />
-            <hr />
+            <p className="title-of-the-app">Registration to Post It</p>
+            <div className="registration-component__symbol-container">
+                <img src={snap} alt="logo" />
+            </div>
+            <input placeholder="E-mail" className="shadow-sm" />
+            <input placeholder="Full name" className="shadow-sm" />
+            <input placeholder="Username" className="shadow-sm" />
+            <input placeholder="Password" className="shadow-sm" />
             <div className="registration-component__button-container--pc">
                 <button className="">Sign-up</button>
             </div>
-            <p className="registration-link">Already registered? <a href="/">Back to Sign-in</a></p>
-            <p className="copyright">&copy; 2022 PostIt</p>
+            <p className="registration-link">Do you have already an account? <a href="/login">Sign-in</a></p>
+            <div className="registration-component__footer-container--pc">
+                <p className="copyright">&copy; 2022 Post It</p>
+            </div>
         </div>
         <div className="d-flex d-sm-none registration-page-container__registration-component--mobile">
-            <p className="title-of-the-app">Register your account to Post It</p>
-            <input placeholder="e-mail, username" />
-            <input placeholder="password" />
-            <input placeholder="password again" />
+            <p className="title-of-the-app">Registration to Post It</p>
+            <div className="registration-component__symbol-container">
+                <img src={snap} alt="logo" />
+            </div>
+            <input placeholder="E-mail" className="shadow-sm" />
+            <input placeholder="Full name" className="shadow-sm" />
+            <input placeholder="Username" className="shadow-sm" />
+            <input placeholder="Password" className="shadow-sm" />
             <div className="registration-component__button-container--mobile">
                 <button className="">Sign-up</button>
             </div>
-            <p className="registration-link">Already registered? <a href="/">Back to Sign-in</a></p>
-            <p className="copyright">&copy; 2022 PostIt</p>
+            <p className="registration-link">Do you have already an account? <a href="/login">Sign-in</a></p>
+            <div className="registration-component__footer-container--mobile">
+                <p className="copyright">&copy; 2022 Post It</p>
+            </div>
         </div>
     </div>
 );
